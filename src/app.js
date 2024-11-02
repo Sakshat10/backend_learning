95c5829464a588107cea930fa72453a720284af1
Sakshat kumar <sakshatkumar007@gmail.com>
2024-11-02 23:05:29 +0530

    fix: did fixes

--- src/app.js
+++ src/app.js
@@ -14,4 +14,15 @@ app.use(express.urlencoded({extended: true, limit: "20kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+// import Routes 
+import userRouter from "./routes/user.routes.js";
+
+
+
+// Routes Declaration
+app.use("/api/v1/users", userRouter);
+
+
+
 export {app}
